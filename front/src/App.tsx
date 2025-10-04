0da2daccf9962ae616a392d01982db9ba4ce4d6d
00.rirififiloulou.00 <00.rirififiloulou.00>
2025-10-04 16:59:03 +0200

    animation omg

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,17 +1,33 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// src/App.tsx
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import Home from "./pages/Home";
 import Sky from "./pages/Sky";
+import PageWrapper from "./components/PageWrapper.tsx";
 
-const App = () => {
+function RoutesWithTransitions() {
+    const location = useLocation();
+
+    useEffect(() => {
+        window.scrollTo({ top: 0, behavior: "instant" as ScrollBehavior });
+    }, [location.pathname]);
+
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/sky" element={<Sky />} />
+        <AnimatePresence mode="wait">
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
+                <Route path="/sky" element={<PageWrapper><Sky /></PageWrapper>} />
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
+        </AnimatePresence>
+    );
+}
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <RoutesWithTransitions />
         </BrowserRouter>
     );
-};
-
-export default App;
+}
