604c0785a6301744d570c671c1f728b3e91aa6dc
Basile Paoli <basile.paoli@gmail.com>
2025-10-04 18:55:42 +0200

    feat: fill sky with stars from api

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,33 +1,53 @@
 // src/App.tsx
 import { useEffect } from "react";
-import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Home from "./pages/Home";
-import Sky from "./pages/Sky";
+import { SkyPage } from "./pages/SkyPage.tsx";
 import PageWrapper from "./components/PageWrapper.tsx";
 
 function RoutesWithTransitions() {
-    const location = useLocation();
+  const location = useLocation();
 
-    useEffect(() => {
-        window.scrollTo({ top: 0, behavior: "instant" as ScrollBehavior });
-    }, [location.pathname]);
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "instant" as ScrollBehavior });
+  }, [location.pathname]);
 
-    return (
-        <AnimatePresence mode="wait">
-            <Routes location={location} key={location.pathname}>
-                <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
-                <Route path="/sky" element={<PageWrapper><Sky /></PageWrapper>} />
-                <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-        </AnimatePresence>
-    );
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <PageWrapper>
+              <Home />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/sky"
+          element={
+            <PageWrapper>
+              <SkyPage />
+            </PageWrapper>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </AnimatePresence>
+  );
 }
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <RoutesWithTransitions />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <RoutesWithTransitions />
+    </BrowserRouter>
+  );
 }
