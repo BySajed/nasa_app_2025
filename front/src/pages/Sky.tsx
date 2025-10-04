3d9db7cc89451c94583c915d5fe21695cec07e90
00.rirififiloulou.00 <00.rirififiloulou.00>
2025-10-04 15:07:47 +0200

    remi routage*2

--- front/src/pages/Sky.tsx
+++ front/src/pages/Sky.tsx
@@ -2,7 +2,7 @@ import img from "../assets/remi.png";
 function Sky() {
     return (
         <div className="w-full h-full flex items-center justify-center">
-            <img src={img} alt="Sky" className=" object-center" />
+            <img src={img} alt="Sky" className="w-1/2 h-1/2 object-contain" />
         </div>
     )
 }
