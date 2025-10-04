7d5976fa559f16ce4d3dce1f54131cc9242ac59e
00.rirififiloulou.00 <00.rirififiloulou.00>
2025-10-04 12:47:23 +0200

    more import

---
+++ front/src/components/SearchBar.tsx
@@ -0,0 +1,66 @@
+"use client";
+import React, { useState } from "react";
+import { AddressAutofill } from "@mapbox/search-js-react";
+import type { SearchBarProps } from "../interfaces/IMap";
+import searchLogo from '../assets/search.svg';
+
+
+const MAPBOX_ACCESS_TOKEN = import.meta.env.MAPBOX_ACCESS_TOKEN;
+
+const SearchBar: React.FC<SearchBarProps> = ({ setCity }) => {
+
+    const [currentSearch, setCurrentSearch] = useState<string | null>(null);
+
+
+    if (!MAPBOX_ACCESS_TOKEN) {
+        console.error("Mapbox access token is not defined");
+    }
+
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (setCity) {
+            setCity(currentSearch || "");
+            setCurrentSearch(null);
+        }
+    };
+
+    return (
+        <div
+            className="p-4 h-16 flex items-center justify-center w-full border border-secondary rounded-full bg-white focus-within:ring-2 focus-within:ring-secondary">
+            <div className="flex items-center justify-center w-14 h-12 bg-secondary rounded-full">
+                <img src={searchLogo} alt="Search icon" className="w-6 h-6" />
+            </div>
+            <div className="w-full">
+                <form onSubmit={handleSubmit} className="flex items-center justify-between w-full">
+                    <AddressAutofill accessToken={NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN!}>
+                        <input
+                            type="text"
+                            required
+                            autoFocus
+                            className="w-full h-full px-4 focus:outline-none"
+                            placeholder="Rechercher votre adresse"
+                            value={currentSearch || ""}
+                            onChange={(e) => {
+                                const value = e.target.value;
+                                setCurrentSearch(value);
+                            }}
+                            autoComplete="address-level2"
+                        />
+                    </AddressAutofill>
+                    {currentSearch && (
+                        <button
+                            type="submit"
+                            className="btn btn-secondary cursor-pointer"
+                        >
+                            Valider
+                        </button>
+                    )}
+                </form>
+            </div>
+
+        </div>
+    );
+};
+
+export default SearchBar;
---
+++ front/src/interfaces/IMap.ts
@@ -0,0 +1,5 @@
+export interface SearchBarProps {
+    searchCity?: boolean;
+    setCity?: (city: string) => void;
+}
+
