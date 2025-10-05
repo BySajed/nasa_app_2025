70826fdabaa8d63e67992acc251a194b989086b3
Dixen-Naxos <armanddailly@gmail.com>
2025-10-05 09:06:56 +0200

    Fix API paths

--- api/src/services/sky_visualization/artificial/tle_cache.py
+++ api/src/services/sky_visualization/artificial/tle_cache.py
@@ -3,7 +3,7 @@ import logging
 import time
 from typing import Dict, Tuple
 from src.dependencies.settings import settings
-from .catalogs_celestrak import fetch_celestrak_all
+from catalogs_celestrak import fetch_celestrak_all
 
 logger = logging.getLogger("api")
 
