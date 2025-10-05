02a9a9c4a55a0536bfe42c077fede6fd1eb87c2a
Basile Paoli <basile.paoli@gmail.com>
2025-10-05 13:06:12 +0200

    feat: add scale size function

--- front/src/components/sky/utils.ts
+++ front/src/components/sky/utils.ts
@@ -1,6 +1,12 @@
 import { Vector3 } from "three";
 
+const SHRINK_FACTOR = 1000;
+
 export function scalePosition([x, y, z]: [number, number, number]): Vector3 {
   const vec = new Vector3(x, y, z);
-  return vec.divideScalar(1000);
+  return vec.divideScalar(SHRINK_FACTOR);
+}
+
+export function scaleSize(size: number): number {
+  return size / SHRINK_FACTOR;
 }
