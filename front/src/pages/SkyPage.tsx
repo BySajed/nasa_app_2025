d16d76bfd9fd88063bd2ab8491079d8049210e09
Sagbyy <sbouhdjeur.gagny@gmail.com>
2025-10-05 11:53:19 +0200

    refactor(SkyPage): remove unused navigation import

--- front/src/pages/SkyPage.tsx
+++ front/src/pages/SkyPage.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { getVisibleStars, type Star } from "../api/sky";
 import { Sky } from "../components/sky/Sky";
 import nasaLogo from "../assets/NASA_logo.svg?url";
-import { useLocation, useNavigate, useNavigation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export function SkyPage() {
   const [stars, setStars] = useState<Star[]>([]);
